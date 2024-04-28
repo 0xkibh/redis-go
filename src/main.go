4f7fdde38cc0ba1c4d083e6b9d2a5cfef407b3b5
0xmatriksh <kishmat17@gmail.com>
2024-04-28 14:52:38 +0545

    feat: setup server and respond to ping
    Setup TCP server with 6379 port, Listen and Accept the connection

--- src/main.go
+++ src/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main(){
-	fmt.Print("Hello World")
+	fmt.Println("Creating Redis Server.")
+	var l,err = net.Listen("tcp",":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close() // close connection once finished
+
+	// infinite loop to read and write response
+	for {
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
